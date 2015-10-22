0f602cb5f7d5b746e8f8878233ea1c4b1e439c8f
Thierry Hubmann <thierry.hubmann@he-arc.ch>
2015-10-22 09:31:19 +0200

    Ajout de données de test

--- ClientsComptes/src/ch/hearc/ig/odi/customeraccount/app/Application.java
+++ ClientsComptes/src/ch/hearc/ig/odi/customeraccount/app/Application.java
@@ -6,6 +6,7 @@
 package ch.hearc.ig.odi.customeraccount.app;
 
 import ch.hearc.ig.odi.customeraccount.business.Account;
+import ch.hearc.ig.odi.customeraccount.business.Bank;
 import ch.hearc.ig.odi.customeraccount.business.Customer;
 
 /**
@@ -14,8 +15,14 @@ import ch.hearc.ig.odi.customeraccount.business.Customer;
  */
 public class Application {
     public static void main(String[] args) {
-
+        Bank bank = new Bank(1, "UBS");
         
+        bank.addCustomer(1, "Thierry", "Hubmann");
+        bank.addCustomer(2, "John", "Bovi");
+        
+        bank.addAccount("001-2001", "Compte épargne", 0.2, bank.getCustomerByNumber(1));
+        
+        System.out.println(bank.getAccountByNumber("001-2001").getName());
         
     }
     
